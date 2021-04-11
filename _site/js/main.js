6337aedec12a8638d1adca33dff402e49195b32a
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2021-04-11 00:40:32 -0400

    layered component

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,1 +1,13 @@
+particlesJS.load("p1", "assets/data/p1.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+particlesJS.load("p2", "assets/data/p2.json", function () {
+  console.log("callback - 2.js config loaded");
+});
+
+particlesJS.load("p3", "assets/data/p3.json", function () {
+  console.log("callback - 3.js config loaded");
+});
+
 console.log("hi");
